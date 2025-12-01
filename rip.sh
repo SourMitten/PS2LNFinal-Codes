d551219879904768833fa11ee4acfe0b2f4ad1f9
SourMitten170 <chris.roffe09@gmail.com>
2025-12-01 13:43:50 -0500

    Add script to create PS2 ISO from disc drive

---
+++ rip.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+
+if [ -b "/dev/sr0" ]; then
+
+    read -p "Enter name for ISO file: " name
+
+    sudo dd if=/dev/sr0 of=~/"$name".iso status=progress
+
+elif [ -b "/dev/sr1" ]; then
+
+    read -p "Enter name for ISO file: " name
+
+    sudo dd if=/dev/sr1 of=~/"$name".iso status=progress
+
+else
+
+    echo "No disc drive found"
+
+fi
