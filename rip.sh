3a39ab91f114952e461cd483c8fb92e9eff1f658
SourMitten170 <chris.roffe09@gmail.com>
2025-12-04 13:30:24 -0500

    Fix ISO file creation before dd command
    Ensure ISO file is created before copying from /dev/sr0.

--- rip.sh
+++ rip.sh
@@ -5,15 +5,15 @@
 if [ -b "/dev/sr0" ]; then
 
     read -p "Enter name for ISO file: " name
-
+    touch ~/Downloads/"$name".iso
     sudo dd if=/dev/sr0 of=~/Downloads/"$name".iso status=progress
 
 elif [ -b "/dev/sr1" ]; then
 
     read -p "Enter name for ISO file: " name
-
-    sudo dd if=/dev/sr1 of=~/Downloads/"$name".iso status=progress
-
+    touch ~/Downloads/"$name".iso
+    sudo dd if=/dev/sr0 of=~/Downloads/"$name".iso status=progress
+    
 else
 
     echo "No disc drive found"
