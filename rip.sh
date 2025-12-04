603ae9c7d733b5b1025dc1d2f35b9e8a3821f03a
SourMitten170 <chris.roffe09@gmail.com>
2025-12-04 13:38:45 -0500

    Fix path for ISO file creation in rip.sh

--- rip.sh
+++ rip.sh
@@ -5,14 +5,14 @@
 if [ -b "/dev/sr0" ]; then
 
     read -p "Enter name for ISO file: " name
-    touch ~/$HOME/Downloads/"$name".iso
-    sudo dd if=/dev/sr0 of=~/$HOME/Downloads/"$name".iso status=progress
+    touch ~/Downloads/"$name".iso
+    sudo dd if=/dev/sr0 of=~/Downloads/"$name".iso status=progress
 
 elif [ -b "/dev/sr1" ]; then
 
     read -p "Enter name for ISO file: " name
-    touch ~/$HOME/Downloads/"$name".iso
-    sudo dd if=/dev/sr0 of=~/$HOME/Downloads/"$name".iso status=progress
+    touch ~/Downloads/"$name".iso
+    sudo dd if=/dev/sr0 of=~/Downloads/"$name".iso status=progress
     
 else
 
